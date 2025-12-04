0c5048c3c212c378514e67b976157871a7dbff01
Yui Oku <Yui.Oku@Ugent.be>
2025-12-04 11:54:42 +0100

    createcomponentfolder

---
+++ components/specificComponents/Event/Event.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from "react";
+
+export default class Event extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				Event:{this.props.blok.title}
+			</div>
+		);
+	}
+}
---
+++ components/specificComponents/StudentLife/StudentLife.js
@@ -0,0 +1,38 @@
+﻿import React, { Component } from "react";
+import css from "./StudentLife.module.scss";
+import Headermenu from "../../genericComponents/Headermenu/Headermenu";
+import Hero from "../../genericComponents/Hero/Hero";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
+import List from "../../genericComponents/List/List";
+
+export default class StudentLife extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div {...storyblokEditable(this.props.blok)}>
+				<Headermenu blok={this.props.menu.content}></Headermenu>
+
+				<main>
+					<Hero blok={this.props.blok} contentTypeTag="course" />
+					<div className={css["studentlife-page__main-content"]}>
+						<div id="studentlife-page__short-description" key="studentlife-page__short-description" className={css["studentlife-page__short-description"]}>
+							<section className={css["rich-text-section--with-navigator"]}>
+								<h2 className={css["rich-text-section__title"]}>StudentLife Details</h2>
+								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
+							</section>
+						</div>
+					</div>
+					{this.props.blok.additionalstuff && this.props.blok.additionalstuff.map((nestedBlok) => (
+							<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+						))}
+				</main>
+			</div>
+		);
+
+	}
+}
