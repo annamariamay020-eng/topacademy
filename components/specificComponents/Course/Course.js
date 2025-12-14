cc041af465ea7f8cf2475c5c233c2e093a327b6f
KakoHiraoka <Kako.Hiraoka@ugent.be>
2025-12-14 16:56:48 +0100

    ok

--- components/specificComponents/Course/Course.js
+++ components/specificComponents/Course/Course.js
@@ -34,10 +34,12 @@ export default class Course extends Component {
 								))}
 							</section>
 						</div>
-						
+				
 						<div id="course-page__short-description" key="course-page__short-description" className={css["course-page__short-description"]}>
 							<section className={css["rich-text-section--with-navigator"]}>
-								
+								{this.props.blok.teachers && this.props.blok.teachers.map((teacher) => (
+									<Location blok={location} key={location._uid} />
+								))}
 							</section>
 						</div>
 						
